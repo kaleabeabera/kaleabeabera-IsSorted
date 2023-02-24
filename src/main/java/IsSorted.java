81b3043e0a4f3e9bef07826c4969e0ed740a4f60
kaleabeabera <Kabera@colgate.edu>
2023-02-24 01:16:55 +0000

    Completed Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,16 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        if (arr == null || arr.length <= 1) {
+            return true;
+        }
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
